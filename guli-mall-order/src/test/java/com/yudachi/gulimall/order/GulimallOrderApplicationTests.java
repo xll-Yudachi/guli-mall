ab62fcadfd20cca44e90c68ae050922a96054a99
yudachi <452419829@qq.com>
2020-05-08 21:09:55 +0800

    项目初始结构创建

---
+++ guli-mall-order/src/test/java/com/yudachi/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yudachi.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
